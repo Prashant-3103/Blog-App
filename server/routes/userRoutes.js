8da1e5affa751ec9fd3147220952c258b3902368
Prashant-3103 <prashantkumarchoudhary.211mn035@nitk.edu.in>
2023-10-09 23:59:36 +0530

    2nd_commit

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const router = express.Router();
+import {loginUser, registerUser, userProfile, updateProfile, updateProfilePicture} from '../controllers/UserController'
+import { authGuard } from '../middleware/authMiddleWare';
+router.post('/register',registerUser)
+router.post('/login',loginUser)
+router.get('/profile', authGuard,userProfile)
+router.put('/updateProfile',authGuard,updateProfile)
+router.put('/updateProfilePicture',authGuard,updateProfilePicture)
+
+export default router
